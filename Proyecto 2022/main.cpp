6b37bfa9348db4c9985505b2ff8214459919ad35
orjuelalaley20 <sebasorjuela@gmail.com>
2022-02-05 12:26:14 -0500

    First Push

---
+++ Proyecto 2022/main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include "Funtions.cxx"
+using namespace std;
+
+int main()
+{
+    // Cadenas
+    char comando[500];
+
+    // Apuntadores
+    char *cad;
+
+    // Vectores
+    vector<string> com;
+
+    do
+    {
+        cout << "$ ";
+        cin.getline(comando, 500);
+
+        cad = strtok(comando, " ");
+        while (cad != NULL)
+        {
+            com.push_back(cad);
+            cad = strtok(NULL, " ");
+        }
+
+        if (com[0] == "cargar_imagen")
+        {
+            Cargar_imagen(com[1]);
+            com.clear();
+        }
+        
+        else if (com[0] == "ayuda")
+        {
+            Ayuda();
+        }
+        else if (com[0] == "salida")
+        {
+            cout << "  Termina la ejecucion de la aplicacion." << endl;
+            com.clear();
+            break;
+        }
+        else
+        {
+            cout << "  comando no existente" << endl;
+            com.clear();
+        }
+    } while (com[0] != "salida");
+
+    return 0;
+}
