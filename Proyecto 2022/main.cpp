5362f3fac5494b7dc552f1ee4f3109060261d11c
orjuelalaley20 <sebasorjuela@gmail.com>
2022-02-11 08:50:45 -0500

    11-02-2022

--- Proyecto 2022/main.cpp
+++ Proyecto 2022/main.cpp
@@ -30,23 +30,28 @@ int main()
         else if (com[0] == "cargar_volumen")
         {
             size = stoi(com[2]);
-            Cargar_Volumen(com[1],size);
+            Cargar_Volumen(com[1], size);
             com.clear();
         }
         else if (com[0] == "info_imagen")
         {
-            
+            Info_imagen();
+            com.clear();
         }
         else if (com[0] == "info_volumen")
         {
+            Info_Volumen();
+            com.clear();
         }
         else if (com[0] == "proyeccion2D")
         {
-  
+            Proyeccion2D(com[1], com[2], com[3]);
+            com.clear();
         }
         else if (com[0] == "ayuda")
         {
             Ayuda();
+            com.clear();
         }
         else if (com[0] == "salida")
         {
